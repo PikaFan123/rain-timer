28bc79f641b4dc567b0466b1a73be21b64584e4c
Pika <lerayplayzz@gmail.com>
2020-08-26 05:15:35 +0200

    Experimental Test if it is currently raining

--- assets/js/modify.js
+++ assets/js/modify.js
@@ -5,19 +5,48 @@ function setTime()
 function timer()
 {
     nextRain = getNextRainTime().getTime()
-
+    rain = false
     // shamelessly stolen from https://www.w3schools.com/howto/howto_js_countdown.asp
+    // rain lasts 1000 seconds
     var x = setInterval(function() {
         var now = Date.now()
 
         var distance = nextRain - now
 
+        if ((distance - 1000) < 3850)
+        {
+            rain = true
+        }
+        else
+        {
+            rain = false
+        }
+
+        if (rain && document.getElementById("qbm").style.backgroundImage != "url(assets/images/bg_rain.png)")
+        {
+            document.getElementById("qbm").style.backgroundImage = "url(assets/images/bg_rain.png)"
+        }
+        else
+        {
+            document.getElementById("qbm").style.backgroundImage = "url(assets/images/bg.png)"
+        }
+        
+
+
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("timer").innerHTML =  hours + "h " + minutes + "m " + seconds + "s ";
-        document.title = "Rain: " +  hours + "h " + minutes + "m " + seconds + "s ";
+        var ta = ""
+        var tta = ""
+        if (rain)
+        {
+            ta = "<br>It's currently raining!"
+            tta = "💦"
+        }
+
+        document.getElementById("timer").innerHTML =  hours + "h " + minutes + "m " + seconds + "s " + ta;
+        document.title = tta + "Rain: " +  hours + "h " + minutes + "m " + seconds + "s ";
         if (distance < 0)
         {
             nextRain = getNextRainTime()
