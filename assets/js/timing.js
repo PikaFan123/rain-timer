f847301b186298c88234f2e173fc081666608fee
Pika <lerayplayzz@gmail.com>
2020-10-26 17:45:47 +0100

    DST Fix

--- assets/js/timing.js
+++ assets/js/timing.js
@@ -1,6 +1,24 @@
+// https://stackoverflow.com/a/11888430
+Date.prototype.stdTimezoneOffset = function () {
+    var jan = new Date(this.getFullYear(), 0, 1);
+    var jul = new Date(this.getFullYear(), 6, 1);
+    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+}
+
+Date.prototype.isDstObserved = function () {
+    return this.getTimezoneOffset() < this.stdTimezoneOffset();
+}
+
 function getNextRainTime()
 {
-    const baseTime = new Date(1596552707 * 1000)
+    
+    ttrem = 3600
+    if (new Date().isDstObserved())
+    {
+        ttrem = 0
+    }
+
+    const baseTime = new Date((1596552707 - ttrem) * 1000)
 
     let nextRain = baseTime
 
